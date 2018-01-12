a3fcc1cbdaaa44de98430f99b1eebc6d438abffd
Dave Parker <dave.x.parker@gmail.com>
2018-01-12 11:39:33 +0000

    Install script passes on CFLAGS, CXXFLAGS, MAKE, CX and CXX to configure.

--- install.sh
+++ install.sh
@@ -6,13 +6,30 @@
 # Only re-run configure if Makefile doesn't already exist
 
 if [ ! -f Makefile ]; then
+
   # Hacking of time-stamps (lost in git) to avoid autoconf issues
   sleep 1; touch aclocal.m4
   sleep 1; touch configure; touch config.h.in; touch `find . -name Makefile.in -print`
+  
+  # Collate arguments to be passed to configure
+  CONFIGURE_ARGS=()
+  if [ ! -z "$CFLAGS" ]; then
+    CONFIGURE_ARGS+=("CFLAGS=$CFLAGS")
+  fi
+  if [ ! -z "$CXXFLAGS" ]; then
+    CONFIGURE_ARGS+=("CXXFLAGS=$CXXFLAGS")
+  fi
+  if [ ! -z "$CC" ]; then
+    CONFIGURE_ARGS+=("CC=$CC")
+  fi
+  if [ ! -z "$MAKE" ]; then
+    CONFIGURE_ARGS+=("MAKE=$MAKE")
+  fi
+  
   # Build Makefile
-  ./configure --prefix="$(pwd)" || exit 1
+  ./configure --prefix="$(pwd)" "${CONFIGURE_ARGS[@]}" || exit 1
+  
 fi
-
 
 # Build, install (locally)
 
