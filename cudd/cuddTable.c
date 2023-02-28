a9ba288a0c64ac0c5cc6e132ca8bf5de1096721f
Dave Parker <dave.x.parker@gmail.com>
2023-02-28 23:49:08 +0000

    Compile fix (finite -> isfinite).
    Previously marked as deprecated, but now giving compiler errors,
    e.g., on new Mac installs.

--- cudd/cuddTable.c
+++ cudd/cuddTable.c
@@ -1507,7 +1507,7 @@ static double truncateDoubleConstant(double value)
 
 	// (round off before doing hash function to ensure
 	//  close valued constants are in the same table)
-	if (finite(value)) {
+	if (isfinite(value)) {
 		trunc = 10000000000.0; // 10^10
 		m = value * trunc;
 		n = floor(m);
